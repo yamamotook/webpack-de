95022ea42bd1d694c51e542786b0471d306b307c
yamamoto <qq931266421@outlook.com>
2021-09-21 15:30:15 +0800

    init

--- loaders/my-style-loader.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (sourceCode) {
-  return `
-    const styleDOM = document.createElement("style");
-    styleDOM.innerHTML = \`${sourceCode}\`;
-    document.head.appendChild(styleDOM);
-    // 还可以输出这一段sourceCode
-    module.exports = \`${sourceCode}\`
-  `;
-};
